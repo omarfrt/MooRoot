78f6b07f939234758b30700f30e7f7659d42530c
omar fertat <omarfertat96@gmail.com>
2024-10-01 14:52:05 +0100

    initial mockup done needs tweeking(links,spaceing,media-queries)

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 "use client"
 import Image from "next/image";
-import styles from "./page.module.css";
 import Styled from "styled-components";
 import moorootSvg from "./mooroot.svg";
 import studioSvg from "./studio.svg";
+const PageWrapper = Styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 0 47px 0 47px ;
+`;
+
+// Add this new styled component for the main content
+const MainContent = Styled.main`
+  flex: 1;
+`;
+
 //LOGO
 const LogoContainer = Styled.div`
   display: flex;
@@ -13,12 +24,10 @@ const LogoContainer = Styled.div`
 `;
 
 const MoorootLogo = Styled(Image)`
-  width: 198px;
-  height: 36px;
+ 
 `;
 const StudioLogo = Styled(Image)`
-  width: 44px;
-  height: 13px;
+  
 `;
 //LINE
 const HorizontalLine = Styled.div`
@@ -26,109 +35,121 @@ const HorizontalLine = Styled.div`
   height: 3px;
   background-color: black;
 `;
-//COLUMN
 const ColumnContainer = Styled.div`
-display: flex;
-justify-content: space-between;
-align-items: left;
-margin-top: 4px;  
-margin-bottom: 10px;
-`
-//First column
-const NameComponent = Styled.div`
-  font-family: 'Space Grotesk', sans-serif;
-  font-weight: ${props => props.fontWeight || 'bold'};
-  font-size: ${props => props.fontSize || '40px'};
-  text-align: left;
-  margin-top: 0px;
-`;
-//Second column
-const AddressContainer = Styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: left;
-  width: 230px;
-  text-align: left;
-  
-`;
-const AddressComponent = Styled.div`
-  font-family: 'Space Grotesk', sans-serif;
-  font-weight: ${props => props.fontWeight || 'bold'};
-  font-size: ${props => props.fontSize || '24px'};
-  text-align: left;
-  
-  
-`;
-//third column
-const WebsiteContainer = Styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: left;
-  width: 311px;
-  height: 62px;
-  text-align: left;
-  
-`;
-const WebsiteComponent = Styled.div`
-  font-family: 'Space Grotesk', sans-serif;
-  font-weight: ${props => props.fontWeight || 'bold'};
-  font-size: ${props => props.fontSize || '24px'};
-   text-align: left;
-   width: 311px;
-  height: 62px;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  margin-top: 5px;
+  margin-bottom: 15px;
 `;
 
-const ThirdColumn = Styled.div`
+const Column = Styled.div`
   display: flex;
   flex-direction: column;
-  align-items: left;
- 
+  align-items: ${props => props.alignItems || 'flex-start'};
+  width: 30%; // Adjust this value as needed
+`;
+
+const TextComponent = Styled.div`
+  font-family: 'Space Grotesk', sans-serif;
+  font-weight: ${props => props.fontWeight || 'medium'};
+  font-size: ${props => props.fontSize || '18px'};
   text-align: left;
 `;
+
+const FooterContainer = Styled.footer`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 20px 0;
+`;
+
+const FooterColumn = Styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const FooterLine = Styled.div`
+  width: 301px;
+  height: 2px;
+  background-color: black;
+  margin-bottom: 10px;
+`;
+
 
 export default function Home() {
   return (
-    <>
-    <LogoContainer >
-      <MoorootLogo src={moorootSvg} alt="mooroot" />
-      <StudioLogo src={studioSvg} alt="studio" />
-    </LogoContainer>
-    <HorizontalLine />
-    <ColumnContainer>
-      <NameComponent>Omar Fertat</NameComponent>
-    <AddressContainer>
-      <AddressComponent>Tangier,</AddressComponent>
-    <AddressComponent> Morocco</AddressComponent>
-    </AddressContainer>
-    <WebsiteContainer>
-      <WebsiteComponent>omarfertat.com,</WebsiteComponent>
-      <WebsiteComponent>omarfertat96@gmail.com</WebsiteComponent>
-    </WebsiteContainer>
-    </ColumnContainer>
-    {/* //second column */}
-    <HorizontalLine />
-    <ColumnContainer>
-      <AddressContainer>
-        <AddressComponent fontWeight="medium" fontSize="24px" >Full Stack /</AddressComponent>
-        <AddressComponent fontWeight="medium" fontSize="24px" >Creative Developer</AddressComponent>
-      </AddressContainer>
-      <AddressContainer>
-        <AddressComponent fontWeight="medium" fontSize="24px" >ReactJs, NextJs,</AddressComponent>
-      <AddressComponent fontWeight="medium" fontSize="24px" > R3F</AddressComponent>
-    </AddressContainer>
-    <WebsiteContainer>
-      <WebsiteComponent fontWeight="medium" fontSize="24px" >NodeJs, Express,</WebsiteComponent>
-      <WebsiteComponent fontWeight="medium" fontSize="24px" >MongoDB</WebsiteComponent>
-    </WebsiteContainer>
-    </ColumnContainer>
-    {/* //third column */}
-    <HorizontalLine />
-    <ColumnContainer>
-    <AddressComponent fontWeight="medium" fontSize="24px" >MexiqueBookShop.com -2022</AddressComponent>
-    <AddressComponent fontWeight="medium" fontSize="24px" >Homylius.lu -2023</AddressComponent>
-    <WebsiteComponent fontWeight="medium" fontSize="24px" >MooRoot.com -?</WebsiteComponent>
-     
-    </ColumnContainer>
-    </>
+    <PageWrapper>
+      <MainContent>
+      <LogoContainer>
+        <MoorootLogo src={moorootSvg} alt="mooroot" />
+        <StudioLogo src={studioSvg} alt="studio" />
+      </LogoContainer>
+      <HorizontalLine />
+      <ColumnContainer>
+        <Column>
+          <TextComponent fontWeight="bold" fontSize="40px">Omar Fertat</TextComponent>
+        </Column>
+        <Column>
+          <TextComponent fontWeight="bold">Tangier,</TextComponent>
+          <TextComponent fontWeight="bold">Morocco</TextComponent>
+        </Column>
+        <Column >
+          <TextComponent fontWeight="bold">omarfertat.com,</TextComponent>
+          <TextComponent fontWeight="bold">omarfertat96@gmail.com</TextComponent>
+        </Column>
+      </ColumnContainer>
+      <HorizontalLine />
+      <ColumnContainer>
+        <Column>
+          <TextComponent>Full Stack /</TextComponent>
+          <TextComponent>Creative Developer</TextComponent>
+        </Column>
+        <Column>
+          <TextComponent>ReactJs, NextJs,</TextComponent>
+          <TextComponent>R3F</TextComponent>
+        </Column>
+        <Column>
+          <TextComponent>NodeJs, Express,</TextComponent>
+          <TextComponent>MongoDB</TextComponent>
+        </Column>
+      </ColumnContainer>
+      <HorizontalLine />
+      <ColumnContainer>
+        <Column>
+          <TextComponent>MexiqueBookShop.com -2022</TextComponent>
+        </Column>
+        <Column>
+          <TextComponent>Homylius.lu -2023</TextComponent>
+        </Column>
+        <Column>
+          <TextComponent>MooRoot.com -?</TextComponent>
+        </Column>
+      </ColumnContainer>
+      </MainContent>
+      <div>{/* Empty div for future content */}</div>
+      
+      <FooterContainer>
+        <FooterColumn>
+          <FooterLine />
+          <TextComponent fontSize="16px">omarfertat.com</TextComponent>
+        </FooterColumn>
+        <FooterColumn>
+          <FooterLine />
+          <TextComponent fontSize="16px">demo1.com</TextComponent>
+          <TextComponent fontSize="16px">demo2.com</TextComponent>
+        </FooterColumn>
+        <FooterColumn>
+          <FooterLine />
+          <TextComponent fontSize="16px">demo1.com</TextComponent>
+          <TextComponent fontSize="16px">demo2.com</TextComponent>
+        </FooterColumn>
+        <FooterColumn>
+          <FooterLine />
+          <TextComponent fontSize="16px">demo1.com</TextComponent>
+          <TextComponent fontSize="16px">demo2.com</TextComponent>
+        </FooterColumn>
+      </FooterContainer>
+    </PageWrapper>
   );  
 }
